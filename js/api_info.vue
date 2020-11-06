8442ed9b5640ff580e5a0c02fa4b33fe93af012c
François de Metz <francois@2metz.fr>
2020-11-06 23:39:28 +0100

    Rework the API view.

--- js/api_info.vue
+++ js/api_info.vue
@@ -3,11 +3,50 @@
     :title="$t('sidebar.api.title')"
     @close="$emit('close')"
   >
-    <p class="px-4 pt-3">{{ $t('sidebar.api.subtitle') }}</p>
-    <i18n path="sidebar.api.content" tag="p" class="text-pre px-4">
-      <a href="https://github.com/indoorequal/mapbox-gl-indoorequal" target="_blank">{{ $t('sidebar.api.plugin') }}</a>
-      <a href="https://indoorequal.com/" target="_blank">{{ $t('sidebar.api.indoorequalcom') }}</a>
-    </i18n>
+    <div class="px-4 pt-4">
+      <p>{{ $t('sidebar.api.subtitle') }}</p>
+      <v-card
+        color="primary"
+        dark
+        href="https://indoorequal.com/"
+        target="_blank"
+        rel="noopener"
+      >
+        <v-card-title>{{ $t('sidebar.api.indoorequal.title') }}</v-card-title>
+        <v-card-subtitle>{{ $t('sidebar.api.indoorequal.subtitle') }}</v-card-subtitle>
+        <v-card-actions>
+          <v-btn text>{{ $t('sidebar.api.indoorequal.cta') }}</v-btn>
+        </v-card-actions>
+      </v-card>
+      <v-card
+        color="secondary"
+        dark
+        class="mt-4"
+        href="https://github.com/indoorequal/mapbox-gl-indoorequal"
+        target="_blank"
+        rel="noopener"
+      >
+        <v-card-title>{{ $t('sidebar.api.mapboxgl.title') }}</v-card-title>
+        <v-card-subtitle>{{ $t('sidebar.api.mapboxgl.subtitle') }}</v-card-subtitle>
+        <v-card-actions>
+          <v-btn text>{{ $t('sidebar.api.mapboxgl.cta') }}</v-btn>
+        </v-card-actions>
+      </v-card>
+      <v-card
+        color="blue darken-3"
+        dark
+        class="mt-4"
+        href="https://indoorequal.com/schema"
+        target="_blank"
+        rel="noopener"
+      >
+        <v-card-title>{{ $t('sidebar.api.schema.title') }}</v-card-title>
+        <v-card-subtitle>{{ $t('sidebar.api.schema.subtitle') }}</v-card-subtitle>
+        <v-card-actions>
+          <v-btn text>{{ $t('sidebar.api.schema.cta') }}</v-btn>
+        </v-card-actions>
+      </v-card>
+    </div>
   </sub-sidebar>
 </template>
 
