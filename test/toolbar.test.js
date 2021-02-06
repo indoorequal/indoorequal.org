53ae590d06990c2b1a974be590cc149bd5c4c4f0
François de Metz <francois@2metz.fr>
2021-02-06 11:44:37 +0100

    Fix async tests.

--- test/toolbar.test.js
+++ test/toolbar.test.js
@@ -19,40 +19,40 @@ describe('Toolbar', () => {
     });
   });
 
-  it('creates the osm.org URL', () => {
-    toolbar.setProps({
+  it('creates the osm.org URL', async () => {
+    await toolbar.setProps({
       mapCenter: { lat: 3, lng: 4 },
       mapZoom: 18
     });
     expect(toolbar.vm.OSMUrl).toEqual('https://www.openstreetmap.org/edit?editor=id#map=18/3/4');
   });
 
-  it('creates the JOSM URL', () => {
-    toolbar.setProps({
+  it('creates the JOSM URL', async () => {
+    await toolbar.setProps({
       mapBounds: [0, 1, 2, 3]
     });
     expect(toolbar.vm.JOSMUrl).toEqual('http://localhost:8111/load_and_zoom?left=0&right=2&top=3&bottom=1');
   });
 
-  it('creates the Vespucci URL', () => {
-    toolbar.setProps({
+  it('creates the Vespucci URL', async () => {
+    await toolbar.setProps({
       mapBounds: [0, 1, 2, 3]
     });
     expect(toolbar.vm.VespucciUrl).toEqual('josm:/load_and_zoom?left=0&right=2&top=3&bottom=1');
   });
 
-  it('creates the OsmInEdit URL', () => {
-    toolbar.setProps({
+  it('creates the OsmInEdit URL', async () => {
+    await toolbar.setProps({
       mapCenter: { lat: 3, lng: 4 },
       mapZoom: 18
     });
     expect(toolbar.vm.OsmInEditUrl).toEqual('https://osminedit.pavie.info/#18/3/4');
   });
 
-  it('creates the OpenLevelUp URL', () => {
-    toolbar.setProps({
+  it('creates the OpenLevelUp URL', async () => {
+    await toolbar.setProps({
       mapCenter: { lat: 3, lng: 4 },
-      mapLevel: 2,
+      mapLevel: '2',
       mapZoom: 18
     });
     expect(toolbar.vm.OpenLevelUpUrl).toEqual('https://openlevelup.net/?l=2#18/3/4');
