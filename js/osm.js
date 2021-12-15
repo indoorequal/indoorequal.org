092a0aab81464409bceca2f8b6a18902fd6558df
François de Metz <francois@2metz.fr>
2021-12-15 19:33:21 +0100

    Use osm2geojson-lite instead of osmtogeojson.

--- js/osm.js
+++ js/osm.js
@@ -1,9 +1,8 @@
-import osmtogeojson from '@map-gl-indoor/osmtogeojson';
+import osm2geojson from 'osm2geojson-lite';
 import { readFileAsText, transformGeoJSON } from './geojson';
 
 export async function transformOSMFile(file) {
   const xmlText = await readFileAsText(file);
-  const xml = new DOMParser().parseFromString(xmlText, 'text/xml');
-  const featureCollection = osmtogeojson(xml);
+  const featureCollection = osm2geojson(xmlText);
   return transformGeoJSON(featureCollection);
 }
