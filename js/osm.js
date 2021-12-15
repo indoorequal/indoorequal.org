e69d3b32432d58313851d893c0fdb05bf032a581
François de Metz <francois@2metz.fr>
2021-12-15 19:40:51 +0100

    Add missing opt on osm2geojson-lite.

--- js/osm.js
+++ js/osm.js
@@ -3,6 +3,6 @@ import { readFileAsText, transformGeoJSON } from './geojson';
 
 export async function transformOSMFile(file) {
   const xmlText = await readFileAsText(file);
-  const featureCollection = osm2geojson(xmlText);
+  const featureCollection = osm2geojson(xmlText, { completeFeature: true });
   return transformGeoJSON(featureCollection);
 }
