be0e462c5f8d2998736792bee4942cf808f9db43
François de Metz <francois@2metz.fr>
2021-12-20 19:58:02 +0100

    Fix preview of osm files.

--- js/osm.js
+++ js/osm.js
@@ -3,6 +3,6 @@ import { readFileAsText, transformGeoJSON } from './geojson';
 
 export async function transformOSMFile(file) {
   const xmlText = await readFileAsText(file);
-  const featureCollection = osm2geojson(xmlText, { completeFeature: true });
+  const featureCollection = osm2geojson(xmlText, { completeFeature: true, renderTagged: true, excludeWay: false });
   return transformGeoJSON(featureCollection);
 }
