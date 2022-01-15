89ae0f4221e427cc63052393acc084ec3f07bc86
François de Metz <francois@2metz.fr>
2022-01-16 00:26:33 +0100

    Update developers sidebar.

--- js/api_info.vue
+++ js/api_info.vue
@@ -4,7 +4,6 @@
     @close="$emit('close')"
   >
     <div class="px-4 pt-4">
-      <p>{{ $t('sidebar.api.subtitle') }}</p>
       <v-card
         color="primary"
         dark
@@ -16,34 +15,6 @@
         <v-card-subtitle>{{ $t('sidebar.api.indoorequal.subtitle') }}</v-card-subtitle>
         <v-card-actions>
           <v-btn text>{{ $t('sidebar.api.indoorequal.cta') }}</v-btn>
-        </v-card-actions>
-      </v-card>
-      <v-card
-        color="secondary"
-        dark
-        class="mt-4"
-        href="https://indoorequal.com/doc/mapbox-gl-indoorequal"
-        target="_blank"
-        rel="noopener"
-      >
-        <v-card-title>{{ $t('sidebar.api.mapboxgl.title') }}</v-card-title>
-        <v-card-subtitle>{{ $t('sidebar.api.mapboxgl.subtitle') }}</v-card-subtitle>
-        <v-card-actions>
-          <v-btn text>{{ $t('sidebar.api.mapboxgl.cta') }}</v-btn>
-        </v-card-actions>
-      </v-card>
-      <v-card
-        color="secondary"
-        dark
-        class="mt-4"
-        href="https://indoorequal.com/doc/openlayers-indoorequal"
-        target="_blank"
-        rel="noopener"
-      >
-        <v-card-title>{{ $t('sidebar.api.openlayers.title') }}</v-card-title>
-        <v-card-subtitle>{{ $t('sidebar.api.openlayers.subtitle') }}</v-card-subtitle>
-        <v-card-actions>
-          <v-btn text>{{ $t('sidebar.api.openlayers.cta') }}</v-btn>
         </v-card-actions>
       </v-card>
       <v-card
