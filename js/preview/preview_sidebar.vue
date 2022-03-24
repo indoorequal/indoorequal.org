827887117603a9a23bc3857ee8493ab87592dac5
François de Metz <francois@2metz.fr>
2022-03-24 18:18:21 +0100

    Add reference about imdf support.

--- js/preview/preview_sidebar.vue
+++ js/preview/preview_sidebar.vue
@@ -5,6 +5,7 @@
   >
     <div class="mx-4">
       <p class="pt-4">{{ $t('preview.description') }}</p>
+      <p>{{ $t('preview.imdf') }}</p>
       <h3 class="text-h5">{{ $t('preview.how.title') }}</h3>
       <ol class="py-3">
         <li>{{ $t('preview.how.content1') }}</li>
