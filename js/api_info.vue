c818e2eafe691f0fdeb841620e64ffb32f45c8e3
François de Metz <francois@2metz.fr>
2022-03-31 09:15:55 +0200

    Add developer card about embedding.

--- js/api_info.vue
+++ js/api_info.vue
@@ -18,6 +18,20 @@
         </v-card-actions>
       </v-card>
       <v-card
+        color="secondary"
+        dark
+        class="mt-4"
+        href="https://github.com/indoorequal/indoorequal.org#remote-command-api"
+        target="_blank"
+        rel="noopener"
+      >
+        <v-card-title>{{ $t('sidebar.api.embed.title') }}</v-card-title>
+        <v-card-subtitle>{{ $t('sidebar.api.embed.subtitle') }}</v-card-subtitle>
+        <v-card-actions>
+          <v-btn text>{{ $t('sidebar.api.embed.cta') }}</v-btn>
+        </v-card-actions>
+      </v-card>
+      <v-card
         color="blue darken-3"
         dark
         class="mt-4"
