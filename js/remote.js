b71ad86eb7b0a0cbbac8536b8d87154c9cbe65cd
François de Metz <francois@2metz.fr>
2025-05-21 19:46:09 +0200

    Update vuetify imports.

--- js/remote.js
+++ js/remote.js
@@ -3,18 +3,18 @@ import 'vuetify/lib/styles/main.sass';
 import { createApp } from 'vue';
 import { createVuetify } from 'vuetify';
 
-import { VApp } from 'vuetify/lib/components/VApp/VApp.mjs';
-import { VBtn } from 'vuetify/lib/components/VBtn/VBtn.mjs';
-import { VMain } from 'vuetify/lib/components/VMain/VMain.mjs';
-import { VFileInput } from 'vuetify/lib/components/VFileInput/index.mjs';
-import { VTextField } from 'vuetify/lib/components/VTextField/index.mjs';
-import { VIcon } from 'vuetify/lib/components/VIcon/VIcon.mjs';
-import { VExpansionPanels } from 'vuetify/lib/components/VExpansionPanel/VExpansionPanels.mjs';
-import { VExpansionPanel } from 'vuetify/lib/components/VExpansionPanel/VExpansionPanel.mjs';
-import { VExpansionPanelTitle } from 'vuetify/lib/components/VExpansionPanel/VExpansionPanelTitle.mjs';
-import { VExpansionPanelText } from 'vuetify/lib/components/VExpansionPanel/VExpansionPanelText.mjs';
-import { VRow } from 'vuetify/lib/components/VGrid/VRow.mjs';
-import { VCol } from 'vuetify/lib/components/VGrid/VCol.mjs';
+import { VApp } from 'vuetify/lib/components/VApp/VApp.js';
+import { VBtn } from 'vuetify/lib/components/VBtn/VBtn.js';
+import { VMain } from 'vuetify/lib/components/VMain/VMain.js';
+import { VFileInput } from 'vuetify/lib/components/VFileInput/index.js';
+import { VTextField } from 'vuetify/lib/components/VTextField/index.js';
+import { VIcon } from 'vuetify/lib/components/VIcon/VIcon.js';
+import { VExpansionPanels } from 'vuetify/lib/components/VExpansionPanel/VExpansionPanels.js';
+import { VExpansionPanel } from 'vuetify/lib/components/VExpansionPanel/VExpansionPanel.js';
+import { VExpansionPanelTitle } from 'vuetify/lib/components/VExpansionPanel/VExpansionPanelTitle.js';
+import { VExpansionPanelText } from 'vuetify/lib/components/VExpansionPanel/VExpansionPanelText.js';
+import { VRow } from 'vuetify/lib/components/VGrid/VRow.js';
+import { VCol } from 'vuetify/lib/components/VGrid/VCol.js';
 
 import { aliases, mdi } from 'vuetify/lib/iconsets/mdi-svg';
 
